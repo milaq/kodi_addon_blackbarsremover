d22f9892e22b9bd840f5ec9007948ad4f723b244
milaq <micha.laqua@gmail.com>
2019-02-21 00:42:15 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,31 @@
+![](resources/icon.png)
+
+# Introduction
+
+Black Bars Remover is a simple addon for Kodi which allows you to scale/fit the playing video to your screen height,
+eliminating all black bars.
+This also takes Kodi's "minimise black bars" setting into consideration. It only zooms the correct amount without
+overscaling.
+
+# Installation
+
+Go to the releases page and download an addon zip to your Kodi machine. You can then install it by using the "install
+from zip file" option.
+
+# Usage
+
+You can easily map the script to one of your remote buttons by modifying/creating
+`.kodi/userdata/keymaps/keyboard.xml`.
+
+E.g. to have the script run on pressing the "Record" button:
+```
+<keymap>
+  <FullscreenVideo>
+    <remote>
+      <record>RunScript(script.video.blackbarsremover,0)</record>
+    </remote>
+  </FullscreenVideo>
+</keymap>
+```
+
+See [https://kodi.wiki/view/Keymap]() for more info.
---
+++ default.py
@@ -0,0 +1,44 @@
+import sys
+import math
+
+import json
+import xbmc
+import xbmcaddon
+import xbmcgui
+
+addon = xbmcaddon.Addon()
+
+
+def notify(text):
+    xbmc.executebuiltin('Notification(' + addon.getAddonInfo('name') + ', ' + text + ')')
+
+
+def apply_zoom_legacy(level):
+    steps = int((level * 100.0) - 100)
+    for _ in range(0, steps):
+        xbmc.executebuiltin('Action(ZoomIn)')
+
+
+blackbarcomp = None
+blackbarcomp_raw = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Settings.GetSettingValue", "params": {"setting": "videoplayer.errorinaspect"}, "id": 1}')
+blackbarcomp_json = json.loads(unicode(blackbarcomp_raw, 'utf-8'))
+if 'result' in blackbarcomp_json:
+    if 'value' in blackbarcomp_json['result']:
+        blackbarcomp = int(blackbarcomp_json['result']['value'])
+
+if not blackbarcomp:
+    notify(addon.getLocalizedString(30003))
+    sys.exit(1)
+
+aspectratio_screen = float(xbmcgui.getScreenWidth()) / float(xbmcgui.getScreenHeight())
+aspectratio_video = xbmc.RenderCapture().getAspectRatio()
+aspectratio_compensation = aspectratio_video - (aspectratio_video * (blackbarcomp / 100.0))
+ratio_correction = (aspectratio_compensation / aspectratio_screen)
+zoomlevel = math.ceil(ratio_correction * 100.0) / 100.0
+
+if zoomlevel <= 1.0:
+    notify(addon.getLocalizedString(30002))
+    sys.exit(1)
+
+notify(addon.getLocalizedString(30001) + ": " + str(zoomlevel))
+apply_zoom_legacy(zoomlevel)
