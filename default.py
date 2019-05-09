d5bb68f57bb5e95e6a74cc7024c1029634f2bd48
milaq <micha.laqua@gmail.com>
2019-05-09 16:48:41 +0200

    add new method to set zoom level for kodi >= 18.2
    see https://github.com/xbmc/xbmc/pull/15477

--- default.py
+++ default.py
@@ -12,12 +12,30 @@ addon = xbmcaddon.Addon()
 def notify(text):
     xbmc.executebuiltin('Notification(' + addon.getAddonInfo('name') + ', ' + text + ')')
 
+def apply_zoom(level):
+    result_raw = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Player.SetViewMode", "params": {"viewmode": {"zoom": ' + str(level) + ' }}, "id": 1}')
+    result_json = json.loads(unicode(result_raw, 'utf-8'))
+    if 'result' in result_json and result_json['result'] == 'OK':
+        return True
+    else:
+        return False
 
 def apply_zoom_legacy(level):
     steps = int((level * 100.0) - 100)
     for _ in range(0, steps):
         xbmc.executebuiltin('Action(ZoomIn)')
 
+def getNewZoomSupported():
+    version_raw = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Application.GetProperties", "params": {"properties": ["version", "name"]}, "id": 1 }')
+    version_json = json.loads(unicode(version_raw, 'utf-8'))
+    if not version_json.has_key('result') and not version_json['result'].has_key('version'):
+        return False
+    if version_json['result']['version']['major'] > 18:
+        return True
+    if version_json['result']['version']['major'] == 18 and version_json['result']['version']['minor'] >= 2:
+        return True
+    else:
+        return False
 
 blackbarcomp = None
 blackbarcomp_raw = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Settings.GetSettingValue", "params": {"setting": "videoplayer.errorinaspect"}, "id": 1}')
@@ -47,5 +65,11 @@ if zoomlevel <= 1.0:
     notify(addon.getLocalizedString(30002))
     sys.exit(1)
 
+if getNewZoomSupported():
+    if not apply_zoom(zoomlevel):
+        notify(addon.getLocalizedString(30004))
+        sys.exit(1)
+else:
+    apply_zoom_legacy(zoomlevel)
+
 notify(addon.getLocalizedString(30001) + " (" + str(int((zoomlevel - 1.0) * 100.0)) + "%)")
-apply_zoom_legacy(zoomlevel)
